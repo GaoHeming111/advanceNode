a8855ca9af4a31aabb39604254e53ac22024ccd0
GaoHeming111 <651812464@qq.com>
2018-09-09 15:45:11 +0800

    笔记

--- server-16-route.js
+++ server-16-route.js
@@ -4,6 +4,7 @@ server.listen(8888)
 var routeUser = express.Router();
 server.use('/user',routeUser)
 
+// 浏览器中输入 http://localhost:8888/user/index.html
 routeUser.get('/index.html',(req,res)=>{
     res.send('index')
 })
