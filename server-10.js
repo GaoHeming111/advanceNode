6d9142ed50153242f32e0fb89f6abd1645cc4a8d
GaoHeming111 <651812464@qq.com>
2018-09-18 09:27:05 +0800

    extend

--- server-10.js
+++ server-10.js
@@ -4,7 +4,7 @@ const server = express()
 
 //有这个中间插件 req.body才生效
 server.use(bodyParser.urlencoded({
-    extended:  false,   //为true表示是拓展模式
+    extended:  false,   //为true表示是拓展模式 返回的对象是一个键值对，当extended为false的时候，键值对中的值就为'String'或'Array'形式，为true的时候，则可为任何数据类型。
     limit: 2*1024*1024      //限制：2M 
 })) 
 
